4f60d139c3b7121b9d52e6093a547e013cca6c6c
Kevin Gayer <kevin@wherewolf.co.nz>
2015-04-11 20:49:57 +1200

    more

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,136 +2,81 @@
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', ['$rootScope', '$state', function($rootScope, $state) {
-    if(_.isEmpty($rootScope.course)) {
-        // No course loaded, fuck off back to the browse page
-        $state.go('app.browse');
-        return;
-    }
+.controller('AppCtrl', [function() {
+
 }])
 
-.controller('BrowseCtrl', ['$scope', '$state', '$rootScope', '$ionicLoading', '$ionicModal', 'DataStore', function($scope, $state, $rootScope,$ionicLoading, $ionicModal, DataStore) {
-
-    $rootScope.course = {};
-    $rootScope.round = [];
-
-    var selectedCourseBeforePotentialAbandon = '';
-
-    $scope.modal = $ionicModal.fromTemplateUrl('templates/newgame.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function(modal) {
-            $scope.modal = modal;
-        });
-
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-    };
-
-    $scope.abandonGame = function() {
-        $rootScope.round.length = 0;
-        $scope.startGame(selectedCourseBeforePotentialAbandon);
-        selectedCourseBeforePotentialAbandon = '';
-        $scope.modal.hide();
-    };
-
-    $scope.continueGame = function() {
-        $scope.modal.hide();
-        $state.go('app.holes');
-    };
-
-    $scope.$on('$destroy', function() {
-        $scope.modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-        // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-        // Execute action
-    });
+.controller('BrowseCtrl', ['GameService', '$scope', '$state', '$ionicLoading', function(GameService, $scope, $state, $ionicLoading) {
 
     $scope.startGame = function(course) {
-        console.log('Starting a new game at ' + course);
-        if (gameInProgress()) {
-            selectedCourseBeforePotentialAbandon = course;
-            $scope.modal.show();
-            return;
-        }
         $ionicLoading.show({template: 'Loading'});
-        DataStore.get(course)
-            .success(function(data) {
-                $rootScope.course = data;
-                $rootScope.round.length = 0;
-                $ionicLoading.hide();
-                $state.go('app.holes');
-            });
-    };
-
-    var gameInProgress = function() {
-        return $rootScope.round && $rootScope.round.length > 0;
+        GameService.downloadCourse(course).then(function() {
+            $ionicLoading.hide();
+            $state.go('app.holes');
+        }, function(error) {
+            console.error('BrowseCtrl                    : ' + error);
+        });
     };
 
 }])
 
-.controller('HolesCtrl', ['$scope', '$rootScope', '$state', function($scope, $rootScope, $state) {
-    if(_.isEmpty($rootScope.course)) {
-        // No course loaded, fuck off back to the browse page
-        $state.go('app.browse');
-        return;
-    }
+.controller('HolesCtrl', ['GameService', '$scope', '$state', function(GameService, $scope, $state) {
 
-    $scope.score = function(holeId) {
-        var thisHole = _.find($rootScope.round, function(hole) { return hole.id === holeId; });
-        if (thisHole) {
-            return ' / Score ' + thisHole.score;
-        }
-    };
+    GameService.getAllHoles().then(function (holes) {
+        $scope.holes = holes;
+    }, function(error) {
+        console.error('HoleCtrl                      : ' + error);
+        console.info ('HoleCtrl                          : redirecting to app.browse');
+        $state.go('app.browse');
+    });
+
 }])
 
-.controller('HoleCtrl', ['$scope', '$rootScope', '$stateParams', '$state', function($scope, $rootScope, $stateParams, $state) {
-
-    if(_.isEmpty($rootScope.course)) {
-        // No course loaded, fuck off back to the browse page
-        $state.go('app.browse');
-        return;
-    }
-
-    $scope.hole = _.findWhere($rootScope.course.holes, {'id': parseInt($stateParams.holeId)});
+.controller('HoleCtrl', ['GameService', '$scope', '$stateParams', '$state', function(GameService, $scope, $stateParams, $state) {
 
     $scope.setScore = function(score) {
-        var thisHole = _.find($rootScope.round, function(hole) { return hole.id === $scope.hole.id; });
-
-        $scope.hole.score = score;
-
-        if (thisHole) {
-            // Hole already recorded, update
-            console.log('Updating hole score');
-            _.each($rootScope.round, function(hole) {
-                if (hole.id === $scope.hole.id) hole.score = score;
-            });
-        } else {
-            // Add
-            console.log('Pushing new hole score');
-            $rootScope.round.push({'id': $scope.hole.id, 'score': score, 'par': $scope.hole.par});
-        }
+        GameService.setScore($scope.holeId, score, $scope.hole.par).then(function(){
+            $scope.hole.score = score;
+        }, function(error) {
+            console.error('HoleCtrl                      : ' + error);
+        });
     };
 
-    $scope.back = function() {
-        var prevHole = _.findWhere($rootScope.course.holes, {'id': (parseInt($stateParams.holeId) - 1)});
-        if (prevHole) {
-            $state.go('app.single', {'holeId': prevHole.id});
-        } else {
+   $scope.back = function() {
+        GameService.getHole(parseInt($stateParams.holeId) - 1).then(function(hole) {
+            $state.go('app.single', {'holeId': hole.id});
+            // return hole;
+        }, function(error) {
+            // Not really an "error", we've run out of holes... Redirect to app.holes
+            console.info ('HolesCtrl                         : ' + error);
+            console.info ('HolesCtrl                         : redirecting to app.holes');
             $state.go('app.holes');
-        }
+        });
+
     };
+
     $scope.next = function() {
-        var nextHole = _.findWhere($rootScope.course.holes, {'id': (parseInt($stateParams.holeId) + 1)});
-        if (nextHole) {
-            $state.go('app.single', {'holeId': nextHole.id});
-        } else {
+        GameService.getHole(parseInt($stateParams.holeId) + 1).then(function(hole) {
+            $state.go('app.single', {'holeId': hole.id});
+            // return hole;
+        }, function(error) {
+            // Not really an "error", we've run out of holes... Redirect to app.holes
+            console.info ('HolesCtrl                         : ' + error);
+            console.info ('HolesCtrl                         : redirecting to app.holes');
             $state.go('app.holes');
-        }
+        });
+
     };
+
+    GameService.getHole($stateParams.holeId).then(function(hole) {
+        $scope.hole = hole;
+        GameService.getScore($stateParams.holeId).then(function(score) {
+            $scope.hole.score = score;
+        });
+    }, function(error) {
+        console.error('HolesCtrl                     : ' + error);
+        console.info ('HolesCtrl                         : redirecting to app.browse');
+        $state.go('app.browse');
+    });
+
 }]);
--- www/js/services.js
+++ www/js/services.js
@@ -2,15 +2,114 @@
 
 angular.module('starter.services', ['ngResource'])
 
-.factory('DataStore', function($http){
+.factory('GameService', function($q, $http) {
+
+    var course = {};
+    var round = [];
+
     return {
-        get: function(course){
-            console.log('DataStore : get(' + course + ')');
-            return $http({
-                    // url: 'http://localhost:5984/course/qtGardens',
-                    url: 'https://discgolfisfun.iriscouch.com/course/' + course,
-                    method: 'GET'
+        downloadCourse: function(courseName) {
+            console.info('GameService.downloadCourse()');
+            var deferred = $q.defer();
+
+            $http.get('https://discgolfisfun.iriscouch.com/course/' + courseName)
+                .success(function(data) {
+                    course = data;
+                    console.info('GameService.downloadCourse()      : ' + courseName + ' downloaded');
+                    deferred.resolve(data);
+                })
+                .error(function(err){
+                    deferred.reject(err);
                 });
+
+            return deferred.promise;
+        },
+        getAllHoles: function() {
+            console.info('GameService.getAllHoles()');
+            var deferred = $q.defer();
+            if (course.holes) {
+                deferred.resolve(course.holes);
+            } else {
+                deferred.reject('No holes found');
+            }
+            return deferred.promise;
+        },
+        getHole: function(holeId) {
+            console.info('GameService.getHole()             : ' + holeId);
+            var deferred = $q.defer(),
+                thisHole = _.find(course.holes, function(hole){ return hole.id === parseInt(holeId); });
+            if (thisHole) {
+                deferred.resolve(thisHole);
+            } else {
+                deferred.reject('Hole not found');
+            }
+            return deferred.promise;
+        },
+        setScore: function(holeId, score, par) {
+            // console.info('GameService.setScore()');
+            var deferred = $q.defer(),
+                thisHole = _.find(round, function(hole){ return hole.id === holeId; });
+
+            if (thisHole) {
+                console.info('GameService.setScore()            : updating score to ' + score);
+                _.each(round, function(hole) {
+                    if (hole.id === holeId) hole.score = score;
+                });
+            } else {
+                console.info('GameService.setScore()            : adding new score of ' + score);
+                round.push({'id': holeId, 'score': score, 'par': par});
+            }
+            deferred.resolve();
+            return deferred.promise;
+        },
+        getScore: function(holeId) {
+            console.info('GameService.getScore()');
+            var deferred = $q.defer(),
+                retVal;
+
+            _.each(round, function(recordedHole) {
+                if (recordedHole.id === parseInt(holeId)) {
+                    // console.log('getScore found score of ' + recordedHole.score);
+                    retVal = recordedHole.score;
+                }
+            });
+            deferred.resolve(retVal);
+            return deferred.promise;
         }
+
+
+        // findAll: function() {
+        //     var deferred = $q.defer();
+        //     deferred.resolve(employees);
+        //     return deferred.promise;
+        // },
+
+        // findById: function(employeeId) {
+        //     var deferred = $q.defer();
+        //     var employee = employees[employeeId - 1];
+        //     deferred.resolve(employee);
+        //     return deferred.promise;
+        // },
+
+        // findByName: function(searchKey) {
+        //     var deferred = $q.defer();
+        //     var results = employees.filter(function(element) {
+        //         var fullName = element.firstName + " " + element.lastName;
+        //         return fullName.toLowerCase().indexOf(searchKey.toLowerCase()) > -1;
+        //     });
+        //     deferred.resolve(results);
+        //     return deferred.promise;
+        // },
+
+        // findByManager: function (managerId) {
+        //     var deferred = $q.defer(),
+        //         results = employees.filter(function (element) {
+        //             return parseInt(managerId) === element.managerId;
+        //         });
+        //     deferred.resolve(results);
+        //     return deferred.promise;
+        // }
+
     };
+
 });
